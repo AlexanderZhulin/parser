1ad2709030b9869b29e7837b889f28f6f7b5d756
Alexander <saszhulin@gmail.com>
2024-08-05 19:09:26 +0300

    Пока только с МарГУ и их таблицей распарсил

---
+++ parser.php
@@ -0,0 +1,34 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
+$client = new Client();
+$response = $client->get('https://marsu.ru/sveden/education/eduChislen.php');
+$html = $response->getBody()->getContents();
+libxml_use_internal_errors(true);
+
+$dom = new \DOMDocument();
+$dom->loadHTML($html);
+$xpath = new DOMXPath($dom);
+
+// $rows = $xpath->evaluate('//tr[@itemprop="eduChislen"]');
+$rows = $xpath->evaluate('//tr[@itemprop="eduChislen"]//th|//tr[@itemprop="eduChislen"]//td');
+
+$countParameters = 0;
+$record = array();
+$records = array();
+
+foreach ($rows as $row) {
+    if ($countParameters >= 13) {
+        $countParameters = 0;
+        $record = [];
+        echo '<br>';
+    }
+    $record[] = $row->textContent;
+    echo $row->textContent . '|';
+    $countParameters++;
+}
+
+var_dump($records);
